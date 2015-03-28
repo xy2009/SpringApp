43fbc9ff7cc44c8d743aeadcf60b7647513f99b2
ShinChven <shinchven@gmail.com>
2015-03-28 20:23:55 +0800

    Initial commit

---
+++ BootSpringWebMVC/src/main/java/net/atlassc/ShinChven/springwebmvc/controller/IndexController.java
@@ -0,0 +1,7 @@
+package net.atlassc.ShinChven.springwebmvc.controller;
+
+/**
+ * Created by ShinChven on 15/3/28.
+ */
+public class IndexController {
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+#SpringBoot test
+Trying a SpringWebmvc based on Spring boot.<p>
+just started, far from finished.
